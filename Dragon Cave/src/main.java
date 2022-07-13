8c7ec00410d459985015468bff666803ec3be89c
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-07-13 11:23:46 -0500

    Dragon Cave
    Upload Project 1

---
+++ Dragon Cave/src/main.java
@@ -0,0 +1,41 @@
+
+import java.util.*;
+
+public class main {
+    public static void main (String [] args){
+
+        final String[] script = {"You are in a land full of dragons. In front of you,"
+                ,"you see two caves. In one cave, the dragon is friendly"
+                ,"and will share his treasure with you. The other dragon"
+                ,"is greedy and hungry and will eat you on sight."
+                ,"Which cave will you go into? (1 or 2)"};
+
+        final String[] optionOne = {"You approach the cave..."
+                ,"It is dark and spooky..."
+                ,"A large dragon jumps out in front of you! He opens his jaws and ..."
+                ,"Gobbles you down in one bite!"};
+        final String[] optionTwo = {"Success! The dragon is friendly and shares his treasure!"};
+
+        Scanner sc = new Scanner(System.in);
+
+
+        for(String s : script){
+            System.out.println(s);
+        }
+        // scanner get user input
+        int choice = sc.nextInt();
+        if(choice == 1){
+            for(String s: optionOne){
+                System.out.println(s);
+            }
+        }
+        else if (choice == 2){
+            for(String s: optionTwo){
+                System.out.println(s);
+            }
+        }
+        else{
+            System.out.println("Wrong choice, start over.");
+        }
+    }
+}
