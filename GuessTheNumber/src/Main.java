faea7fb93f269ff8e4847b49e1fea2bdb9169c0e
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-07-14 18:40:01 -0500

    comments

--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -4,18 +4,21 @@ public class Main {
     public static void main (String[] args){
         int max = 20;
         int min = 0;
+
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Hello! What is your name?");
 
         String name = sc.nextLine();
 
+        //get random number in range of 0 - 20
         System.out.println("Well, "+ name +", I am thinking of a number between 1 and 20");
         System.out.println("Take a guess");
 
 
         int random = (int) ((Math.random() * (max -min)) + min);
         int guess = 0;
+        // loop until user guess random number
         while(true){
             ++guess;
             int choice = sc.nextInt();
@@ -24,6 +27,7 @@ public class Main {
                 System.out.println("Would you like to play again? (y or n)");
                 char c = sc.next().charAt(0);
                 if(c == 'y'){
+                    //call main in user wants a new game
                     main(null);
                 }
                 else{
