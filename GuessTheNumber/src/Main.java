eb6e7feb903f42c59beb3577be849d12fed337b4
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-07-20 20:20:44 -0500

    Added try catch blocks

--- GuessTheNumber/src/Main.java
+++ GuessTheNumber/src/Main.java
@@ -6,10 +6,14 @@ public class Main {
         int min = 0;
 
         Scanner sc = new Scanner(System.in);
+        String name = "";
 
-        System.out.println("Hello! What is your name?");
-
-        String name = sc.nextLine();
+        try {
+            System.out.println("Hello! What is your name?");
+            name = sc.nextLine();
+        } catch (Exception e){
+            System.out.println("Error at user name input!");
+        }
 
         //get random number in range of 0 - 20
         System.out.println("Well, "+ name +", I am thinking of a number between 1 and 20");
@@ -23,16 +27,37 @@ public class Main {
         // loop until user guess random number
         while(true){
             ++guess;
-            int choice = sc.nextInt();
+            int choice = 0;
+
+            //Catch wrong input exceptions.
+            while(!sc.hasNextInt()){
+                try{
+                    choice = sc.nextInt();
+                }catch (Exception e){
+                    sc.next(); // advance past bad input
+                    System.out.println("Error! Input only numbers");
+                }
+            }
+            choice = sc.nextInt(); // scanner has int skip while block
+
             if(choice == random){
                 System.out.println("Good job, "+name+"! You guessed my number in "+guess+" guesses!");
                 System.out.println("Would you like to play again? (y or n)");
-                char c = sc.next().charAt(0);
+                char c = 'a';
+
+                try {
+                    c = sc.next().charAt(0);
+                }catch (Exception e){
+                    sc.next();
+                    System.out.println("Error! Input only letters!");
+                }
+                
                 if(c == 'y'){
                     //call main if user wants a new game
                     main(null);
                 }
                 else{
+                    System.out.println("Game Over.");
                     break;
                 }
 
