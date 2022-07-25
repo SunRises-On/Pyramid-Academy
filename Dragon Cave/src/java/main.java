7959f3cd4654387083a010133fc939e6cbc26953
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-07-24 19:34:44 -0500

    Refractored code and added JUnit testing

--- Dragon Cave/src/java/main.java
+++ Dragon Cave/src/java/main.java
@@ -1,20 +1,22 @@
+//package src/java/main.java;
+//package java.main;
 
 import java.util.*;
 
 public class main {
+    static final String[] script = {"You are in a land full of dragons. In front of you,"
+            ,"you see two caves. In one cave, the dragon is friendly"
+            ,"and will share his treasure with you. The other dragon"
+            ,"is greedy and hungry and will eat you on sight."
+            ,"Which cave will you go into? (1 or 2)"};
+
+    static final String[] optionOne = {"You approach the cave..."
+            ,"It is dark and spooky..."
+            ,"A large dragon jumps out in front of you! He opens his jaws and ..."
+            ,"Gobbles you down in one bite!"};
+    static final String[] optionTwo = {"Success! The dragon is friendly and shares his treasure!"};
     public static void main (String [] args){
 
-        final String[] script = {"You are in a land full of dragons. In front of you,"
-                ,"you see two caves. In one cave, the dragon is friendly"
-                ,"and will share his treasure with you. The other dragon"
-                ,"is greedy and hungry and will eat you on sight."
-                ,"Which cave will you go into? (1 or 2)"};
-
-        final String[] optionOne = {"You approach the cave..."
-                ,"It is dark and spooky..."
-                ,"A large dragon jumps out in front of you! He opens his jaws and ..."
-                ,"Gobbles you down in one bite!"};
-        final String[] optionTwo = {"Success! The dragon is friendly and shares his treasure!"};
 
         Scanner sc = new Scanner(System.in);
 
@@ -24,19 +26,28 @@ public class main {
         }
         // scanner get user input
         int choice = 0;
+        choice = getInput(sc);
 
+        playGame(choice);
+    }
+
+    //method to get input from scanner
+    static int getInput (Scanner sc){
+        int input = -1 ;
         //Added try catch block for Project 3
         while(!sc.hasNextInt()) {  //if scanner doesn't have int repeat
             try {
-                choice = sc.nextInt();
+                input = sc.nextInt();
             } catch (Exception e) {
                 sc.next(); //advance past bad input
                 System.out.println("Error! Input only numbers.");
             }
         }
-        choice = sc.nextInt(); //scanner has int next proceed
-
-
+        input = sc.nextInt(); //scanner has int next proceed
+        return input;
+    }
+    //Method to play game, depending on user input of one, two or other.
+    static void playGame(int choice){
         if(choice == 1){
             for(String s: optionOne){
                 System.out.println(s);
@@ -51,5 +62,4 @@ public class main {
             System.out.println("Wrong choice, start over.");
         }
     }
-
 }
--- Dragon Cave/src/test/mainTest.java
+++ Dragon Cave/src/test/mainTest.java
@@ -1,4 +1,35 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+import java.util.Scanner;
+
 class mainTest {
-  
+
+    main Main;
+
+    @BeforeEach
+    void setUp() {
+        Main = new main();
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void main() {
+        String data = "1";
+        InputStream stdin = System.in;
+        System.setIn(new ByteArrayInputStream(data.getBytes()));
+        Scanner scan = new Scanner(System.in);
+        System.setIn(stdin);
+        int choice;
+        choice = main.getInput(scan);
+        Assertions.assertEquals(choice = 1, 1, "choice does not equal 1");
+    }
+    
 }
