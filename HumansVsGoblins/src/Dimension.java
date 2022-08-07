ac9320396f5c3c800cbb4708e752a010ca98d341
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-08-06 21:10:52 -0500

    Delete commits.

--- HumansVsGoblins/src/Dimension.java
+++ HumansVsGoblins/src/Dimension.java
@@ -32,12 +32,6 @@ public class Dimension extends Sprite implements Commons{
         int x = pos[0];
         int y = pos[1];
         int i = 0;
-        if(x >= BOARD_WIDTH || y>= BOARD_HEIGHT){
-           //throw an exception
-        }
-        if(x < 0 || y < 0){
-            //throw an exception
-        }
         Character c = screen.get(x).get(y);
         if(c != '0'){
             return false;
@@ -45,40 +39,12 @@ public class Dimension extends Sprite implements Commons{
 
         return true;
     }
-    public boolean setIndex (int[] pos, Character image){
+    public void setIndex (int[] pos, Character image){
         int x = pos[0];
         int y = pos[1];
         int i = 0;
         screen.get(y).set(x, image);
-        return true;
     }
 }
 
 
-/*
-* // Online Java Compiler
-// Use this editor to write, compile and run your Java code online
-import java.util.*;
-public class HelloWorld {
-    public static void main(String[] args) {
-        List<List<String>> screen = new ArrayList<List<String>>();
-        List<String> row = new ArrayList<String>();
-        int x = 2;
-        int y = 2;
-
-        for(int i = 0 ; i < x  ; ++i){
-            row.add("0");
-        }
-        //add rows
-        for(int i = 0; i <y ; ++i){
-            screen.add(row);
-        }
-        for(List<String> inner : screen){
-            System.out.println(Arrays.toString(screen.toArray()));
-        }
-        System.out.println("??????");
-        //String formattedString = screen.toString().replace(",", "") .replace("[", "")  .replace("]", "") ;
-      //  System.out.println(screen);
-    }
-}
-* */
