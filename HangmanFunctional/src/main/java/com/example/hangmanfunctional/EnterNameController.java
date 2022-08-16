ba488325ca81070f9d2ffe9f1c1ebb346cd2f597
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-08-16 15:07:35 -0500

    Add new stages.

---
+++ HangmanFunctional/src/main/java/com/example/hangmanfunctional/EnterNameController.java
@@ -0,0 +1,25 @@
+package com.example.hangmanfunctional;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class EnterNameController {
+
+    @FXML
+    TextField nameTextField;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void save(ActionEvent event) throws IOException{
+        //get text from text field
+        String username = nameTextField.getText();
+
+    }
+}
---
+++ HangmanFunctional/src/main/java/com/example/hangmanfunctional/MenuController.java
@@ -0,0 +1,43 @@
+package com.example.hangmanfunctional;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+public class MenuController {
+    @FXML
+    ImageView hangmanImage;
+    @FXML
+    Button startButton;
+    @FXML
+    Button scoreButton;
+    @FXML
+    Label titleLabel;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void login (ActionEvent event) throws IOException{
+        FXMLLoader loader = new FXMLLoader(getClass().getResource("EnterName.fxml"));
+        root = loader.load();
+
+        EnterNameController controller = loader.getController();
+
+        stage = (Stage)((javafx.scene.Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+
+    }
+}
