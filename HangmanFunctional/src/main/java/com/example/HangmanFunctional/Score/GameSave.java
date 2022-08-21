9d637f346e856f78f0c2bdd6e78f13e01e588656
Jewelle Wilson <jewellelenicewilson@gmail.com>
2022-08-21 12:29:01 -0500

    Refractor.

--- HangmanFunctional/src/main/java/com/example/HangmanFunctional/Score/GameSave.java
+++ HangmanFunctional/src/main/java/com/example/HangmanFunctional/Score/GameSave.java
@@ -1,2 +1,13 @@
-package com.example.HangmanFunctional.Score;public class Player {
+package com.example.HangmanFunctional.Score;
+
+public class GameSave {
+    private int score;
+    private String name;
+    GameSave(int newScore, String newName){
+        this.score = newScore;
+        this.name = newName;
+    }
+
+    public int getScore(){ return score; }
+    public String getName(){ return name;}
 }
--- HangmanFunctional/src/main/java/com/example/HangmanFunctional/Score/ScoreFile.java
+++ HangmanFunctional/src/main/java/com/example/HangmanFunctional/Score/ScoreFile.java
@@ -2,9 +2,7 @@ package com.example.HangmanFunctional.Score;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 public class ScoreFile {
     private static final String filePath= "src/main/resources/Leaderboard.txt" ;
@@ -12,18 +10,36 @@ public class ScoreFile {
     private Scanner scanner;
 
     protected int getRank (int userScore, String userName) throws NumberFormatException{
+
+            //Key = Rank,  Value = Player which has score and name
+            Map<Integer, List<GameSave>> save = new TreeMap<>();
+            int userRank = 0;
+            save = readFile(save);
+
+            if(save.isEmpty()){
+
+                userRank = 1;
+                GameSave gameSave = new GameSave(userScore, userName);
+
+                List<GameSave> newList = new ArrayList<>();
+                newList.add(gameSave);
+
+                save.put( userRank, newList );
+            }
+
+
+              
+            //search gameSave for our score
+            //if found get
+
+        return -1;
+    }
+
+    private  Map<Integer, List<GameSave>> readFile ( Map<Integer, List<GameSave>> save  ) throws NumberFormatException{
         try{
 
             File text = new File(filePath);
             Scanner scanner = new Scanner(text);
-            //Key, Value
-            //Key = Rank,  Value = Player which has score and name 
-            Map<Integer, ArrayList<Player>> user ;
-            Map<Integer, String> scoreMap ;
-
-            int userRank, prevRank, prevScore;
-            userRank = prevRank = prevScore = 0;
-            int lineNumber = 1;
 
             while(scanner.hasNextLine()){
 
@@ -32,63 +48,28 @@ public class ScoreFile {
                 if(line.isEmpty()){
                     break;
                 }
-                //rank name score
+                //Line is ordered by String, rank name score
                 String[] array = line.split(" "); /*split by space */
 
                 int currRank = Integer.parseInt(array[0]);
                 String currName = array[1];
                 int currScore = Integer.parseInt(array[2]);
-                //60    userScore = 60
-                if(currScore == userScore){
-                    userRank = currRank;
-                    break;
-                }
-                // 50    userScore = 100
-                if( (prevScore == 0) && ( currScore < userScore  )){
-                    userRank = currRank;
-                    decrementScore( lineNumber );
-                }
-                //100    userScore = 50;
-                if( (prevScore == 0 ) && ( currScore > userScore)){
-                    userRank = currRank++;
-                    break;
-                }
-                //100 50       userScore = 60
-                if( (prevScore > userScore) && (currScore < userScore ) ){
-                    userRank = currRank;
-                    decrementScore( lineNumber );
-                    break;
-                }
-                // 100 90  userScore = 60  (do nothing)
 
-                prevRank = currRank;
-                prevScore = currScore;
+                GameSave gameSave = new GameSave(currScore, currName);
 
-                lineNumber++;
-            }
-            /* Empty file */
-            if(lineNumber == 1){
-                userRank = 1;
-            }
-            /* User score is the lowest ever */
-            if( userRank == 0){
-                userRank = prevRank++;
-            }
+                if( !save.containsKey(currRank)){
+                    List<GameSave> newList = new ArrayList<>();
+                    newList.add(gameSave);
+                    save.put( currRank, newList );
+                }else{
+                    save.get(currRank).add(gameSave);
+                }
 
+            }
             scanner.close();
-
-            return userRank;
-
         }catch( FileNotFoundException e){
             System.out.println("Leaderboard.txt not found");
         }
-        return -1;
-    }
-    //get rank
-    //get file position
-    //sort file
-    private void decrementScore(int lineNum){
-        int i = 1;
-
+        return save;
     }
 }
